007e6e2607601569a0aab1a3eb9419a585261834
Will Fuqua <wafuqua@gmail.com>
2013-08-13 19:10:13 -0400

    use set to store user votes
    Prevents a user from voting more than once for a given restaurant

--- src/lunchpick/vote_manager.clj
+++ src/lunchpick/vote_manager.clj
@@ -1,9 +1,10 @@
-(ns lunchpick.vote-manager)
+(ns lunchpick.vote-manager
+  (require [clojure.set :refer [union]]))
 
 (def votes (atom {})) ;durable persistence is for sissies. Map of restaurant->[usernames]
 
 (defn add-vote [restaurant user]
-  (swap! votes update-in [restaurant] conj user))
+  (swap! votes update-in [restaurant] union #{user}))
 
 (defn remove-vote [restaurant user]
   (swap! votes update-in [restaurant] disj user))
